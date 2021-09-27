d44543be0a177db3b4ef8add462590449fccef10
chrisG <chris.grd.alt@gmail.com>
2021-09-27 21:33:58 +0700

    dd

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const config = {
         name: 'squido FAQ',
         description: 'An FAQ theme for squido',
         twitterHandle: '@c',
-        baseUrl: 'https://chrgrd1818.github.io/squido-app/',
+        baseUrl: 'https://chrgrd1818.github.io/squido-app',
         sourcesExt: 'markdown',
         summaryLength: 250,
         sourceDir: 'source',
